b0326c747d8d533701ef3370efb770eb85583e53
Andy <andyerickson58201@gmail.com>
2022-11-24 12:48:35 -0600

    output inital nmap scan cleaned to variables in loop

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import subprocess
+import re
 
+#returns local wifi IP from substring of return from ipconfig on windows
 def getIPv4(): 
     ipScan = subprocess.Popen(["ipconfig"], stdout=subprocess.PIPE).communicate()[0]
     string = ipScan.decode()
@@ -21,7 +23,7 @@ def getIPv4():
                 continue
             ip += digit 
         
-    print("Local IP : ",ipArr[0])
+    print(f"Local IP : {ipArr[0]}\n")
     temp = ipArr[0]
     count = 0
     for i in range(len(temp)-1, 0, -1):
@@ -31,9 +33,38 @@ def getIPv4():
         
 ipRange, count = getIPv4()
 
-scan = subprocess.Popen(["nmap", "-sn", ipRange[:-count]+"1-255"], stdout=subprocess.PIPE).communicate()[0]
-print(scan)
-
+scan = subprocess.Popen(["nmap", "-sn", ipRange[:-count]+"1-255", "--exclude", ipRange], stdout=subprocess.PIPE).communicate()[0]
+string = scan.decode("UTF-8")
+#print(scan)
+startIndexes = [i.start() for i in re.finditer("Nmap scan report for", string)]
+names = []
+for i in startIndexes:
+    end = 0
+    mac = None
+    sub = string[i+21:]
+    sub = sub.replace("\r", '')
+    end = sub.find("Nmap scan report for")
+    if end == -1:
+        end = sub.find("Nmap done:")
+    #print(sub[:end])
+    sub = sub[:end]
+    if sub.find("\n") < sub.find('('):
+        name = sub[:sub.find("\n")]
+        ip = name
+    else:
+        name = sub[:sub.find('(')]
+        ip = sub[len(name)+1:sub.find(')')]
+    macIndex = sub.find("MAC Address:")
+    if macIndex != -1:
+        mac = sub[macIndex+13:sub.find(' ', macIndex+13)]
+        deviceType = sub[sub.find('(',macIndex+13)+1:sub.find(')',macIndex+13)]
+    print(f"Name: {name}")
+    print(f"IP: {ip}")
+    print(f"MAC Address: {mac}")
+    print(f"Device Type: {deviceType}")
+    print()
+               
+    
     
 
 
